7d52eeff2eb313c97db9166c04a4f72bc3ddcc2d
ARMSantz <arthurasdsa@gmail.com>
2026-07-07 23:54:51 -0300

    revert: restore fork web version of CardObjectController (BotDeckData)

--- Assets/Scripts/Script/CardObjectController.cs
+++ Assets/Scripts/Script/CardObjectController.cs
@@ -22,11 +22,7 @@ public class CardObjectController : MonoBehaviour
 
         if (GManager.instance.IsAI)
         {
-            if (ContinuousController.instance.AIBattleDeckData != null)
-            {
-                RandomDeck = ContinuousController.instance.AIBattleDeckData;
-            }
-            else if (ContinuousController.instance.DeckDatas.Count((deckData) => deckData.IsValidDeckData()) > 0)
+            if (ContinuousController.instance.DeckDatas.Count((deckData) => deckData.IsValidDeckData()) > 0)
             {
                 List<DeckData> deckDatas = new List<DeckData>();
 
@@ -38,7 +34,9 @@ public class CardObjectController : MonoBehaviour
                     }
                 }
 
-                DeckData randomDeck = deckDatas[UnityEngine.Random.Range(0, deckDatas.Count)];
+                DeckData randomDeck = (ContinuousController.instance.BotDeckData != null && ContinuousController.instance.BotDeckData.IsValidDeckData())
+                    ? ContinuousController.instance.BotDeckData
+                    : deckDatas[UnityEngine.Random.Range(0, deckDatas.Count)];
 
                 RandomDeck = new DeckData(randomDeck.GetThisDeckCode(), randomDeck.DeckID);
 
